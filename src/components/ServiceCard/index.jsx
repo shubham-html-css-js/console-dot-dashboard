faf9e4e4b055411e95099c67029ceb9a35eefdd7
shubham-html-css-js <shubh8221a@gmail.com>
2022-11-10 17:55:31 +0530

    Added result link to the dashboard

--- src/components/ServiceCard/index.jsx
+++ src/components/ServiceCard/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './index.css'
 const ServiceCard = ({cardContent}) => {
   return (
-    <div className='service-card' style={{backgroundColor:cardContent._source.result=='FAIL'?'red':'green'}}>
+    <div className='service-card' style={{backgroundColor:cardContent._source.result==='FAIL'?'red':'green'}}>
         <div className='card-Title'>
             {cardContent._source.product}
         </div>
@@ -11,6 +11,9 @@ const ServiceCard = ({cardContent}) => {
         </div>
         <div className='card-result'>
         Result : {cardContent._source.result}
+        </div>
+        <div className='card-link'>
+        <a href={cardContent._source.link} className='result-link'>Result Link</a>
         </div>        
         </div>
   )
